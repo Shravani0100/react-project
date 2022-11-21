090028e15aebf52b3109cbe3b24319e2c3081241
Shravani0100 <shravanitamhan@1998.gmail.com>
2022-11-21 23:12:13 +0530

    nav bar addition

--- disney-clone/src/components/Login.js
+++ disney-clone/src/components/Login.js
@@ -1,16 +1,23 @@
 import styled from 'styled-components'
 
 const Login = (props) => {
-    return (
-        <Container>
-            <Content>
-                <CTA>
-                    <CTALogoOne src="/images/cta-logo-one.svg" alt=' ' />
-                </CTA>
-                <BgImage />
-            </Content>
-        </Container>
-    );
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt=' ' />
+          <SignUp>Get All There</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
 };
 
 const Container = styled.section`
@@ -21,7 +28,7 @@ const Container = styled.section`
     height: 100vh;
 `;
 
-const Content= styled.div`
+const Content = styled.div`
     margin-bottom: 10vw;
     width: 100%;
     position: relative;
@@ -60,6 +67,38 @@ const CTALogoOne = styled.img`
   max-width: 600px;
   min-height: 1px;
   display: block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
   width: 100%;
 `;
 
